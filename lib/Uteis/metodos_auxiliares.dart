e5460a7ee18b9d60d8d2e7a574c6aa8c1fcfc2de
jhona <jhonatansbestevao@gmail.com>
2024-08-21 18:39:14 -0300

    add mais index para cortar letra ao usar a tela de criacao por texto

--- lib/Uteis/metodos_auxiliares.dart
+++ lib/Uteis/metodos_auxiliares.dart
@@ -10,13 +10,9 @@ class MetodosAuxiliares {
       String versoConcatenado = "";
       for (int index = 0; index < corte.length; index++) {
         versoConcatenado =
-        "$versoConcatenado ${Constantes.stringPularLinhaSlide} ${corte
-            .elementAt(index)}";
-
+            "$versoConcatenado ${Constantes.stringPularLinhaSlide} ${corte.elementAt(index)}";
         // verificando se index da lista e igual a algum dos valores passados para adicionar
         // string na outra lista pegando 2 linhas por vez lembrando 0 conta
-        //print("s@${corte.elementAt(index)}");
-        //print("#$versoConcatenado");
         if (index == 1 ||
             index == 3 ||
             index == 5 ||
@@ -37,7 +33,41 @@ class MetodosAuxiliares {
             index == 35 ||
             index == 37 ||
             index == 39 ||
-            index == 41) {
+            index == 41 ||
+            index == 43 ||
+            index == 45 ||
+            index == 47 ||
+            index == 49 ||
+            index == 51 ||
+            index == 53 ||
+            index == 55 ||
+            index == 57 ||
+            index == 59 ||
+            index == 61 ||
+            index == 63 ||
+            index == 65 ||
+            index == 67 ||
+            index == 69 ||
+            index == 71 ||
+            index == 73 ||
+            index == 75 ||
+            index == 77 ||
+            index == 79 ||
+            index == 81 ||
+            index == 83 ||
+            index == 85 ||
+            index == 87 ||
+            index == 89 ||
+            index == 91 ||
+            index == 93 ||
+            index == 95 ||
+            index == 97 ||
+            index == 99 ||
+            index == 101 ||
+            index == 103 ||
+            index == 105 ||
+            index == 107 ||
+            index == 109) {
           letraCompletaCortada.add(versoConcatenado);
           versoConcatenado = "";
         } else if (index == corte.length - 1) {
